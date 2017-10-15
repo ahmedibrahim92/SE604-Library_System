b54f1e64d8c5a46312e25ff4e48408847165ebda
Ibrahim, Ahmed <Ahmed_Ibrahim@mentor.com>
2017-10-15 11:11:29 +0200

    update db connection sever

--- member/dbcon.php
+++ member/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-mysql_select_db('sql11198356',mysql_connect('sql11.freesqldatabase.com','sql11198356','TmUcYKGchI'))or die(mysql_error());
+mysql_select_db('se406',mysql_connect('mysql6.gear.host','se406','Ru0k_b7680V~'))or die(mysql_error());
 ?>
